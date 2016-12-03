ccfcce95046df34d9e9e06700ff87cd1252a744b
alpsleonardo <alpsleonardo@gmail.com>
2016-12-02 23:51:09 -0500

    App working

--- www/js/app.js
+++ www/js/app.js
@@ -41,6 +41,16 @@ angular.module('starter', ['ionic', 'ngCordovaOauth', 'ngStorage', 'starter.cont
     }
   })
 
+
+  .state('app.login', {
+     url: '/login',
+     views: {
+       'menuContent': {
+         templateUrl: 'templates/login.html'
+       }
+     }
+  })
+
   .state('app.browse', {
       url: '/browse',
       views: {
@@ -69,5 +79,5 @@ angular.module('starter', ['ionic', 'ngCordovaOauth', 'ngStorage', 'starter.cont
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/login');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $rootScope, $ionicModal, $timeout, $cordovaOauth, $localStorage) {
+.controller('AppCtrl', function($scope, $ionicModal, $cordovaOauth, $timeout, $localStorage, $location){
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -9,7 +9,6 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  // Form data for the login modal
   $scope.loginData = {};
 
   // Create the login modal that we will use later
@@ -19,76 +18,66 @@ angular.module('starter.controllers', [])
     $scope.modal = modal;
   });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
+
+  // Triggered in the login modal to close it 
+  $scope.closeLogin = function() { 
+    $scope.modal.hide(); 
   };
 
-  // Open the login modal
-  $rootScope.login = function() {
-    $scope.modal.show();
+  $scope.$on('openLogin', function(){
+    // alert("Called the event openLogin");
+    $scope.login();
+  });
+
+  // Open the login modal 
+  $scope.login = function() { 
+    // alert("You called login");
+    $scope.modal.show(); 
   };
 
-  // $scope.on('redirectToLoginPage', function(event, data) {
-  //   $scope.login();
-  // });
 
   $scope.loginFB = function () {
     $cordovaOauth.facebook(1185817128161808, ["email", "public_profile"]).then(function (result) {
       $localStorage.accessToken = result.access_token;
-      // $location.path("/app/playlists");
+      $location.path("/app/playlists");
     }, function (error) {
       alert(error);
       console.log(error);
     });
   };
 
-  $scope.logout = function () {
-    delete $localStorage.accessToken;
-    $scope.login();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+  $scope.logout = function () { 
+    delete $localStorage.accessToken; 
+    $scope.login(); 
   };
 })
 
-.controller('PlaylistsCtrl', function($scope, $rootScope, $stateParams, $http, $localStorage) {
-  // $scope.loginPage = LoginPage;
+.controller('PlaylistsCtrl', ['$scope', '$stateParams', '$http', '$localStorage',
+  function($scope, $stateParams, $http, $localStorage, $location) {
+    $scope.init = function () {
+      if ($localStorage.hasOwnProperty("accessToken") === true) {
+        $http.get("https://graph.facebook.com/v2.8/me", {
+          params: {
+            access_token: $localStorage.accessToken,
+            fields: "id, cover, name, first_name, last_name, age_range, link, gender, locale, picture, timezone, updated_time, verified, email",
+            format: "json"
+          }
+        }).then(function (result) {
+          alert("You are logged in!");
+          $scope.profileData = result.data;
+          $location.path('/app/playlists');
+        }, function (error) {
+          alert("There was a problem getting your profile.");
+          console.log(error);
+        });
+      } else {
+        alert("You are not logged in!");
+        $scope.$emit('openLogin', []);
+      }
+    };
+}]);
 
-  $scope.init = function () {
-    if ($localStorage.hasOwnProperty("accessToken") === true) {
-      $http.get("https://graph.facebook.com/v2.8/me", {
-        params: {
-          access_token: $localStorage.accessToken,
-          fields: "id, cover, name, first_name, last_name, age_range, link, gender, locale, picture, timezone, updated_time, verified, email",
-          format: "json"
-        }
-      }).then(function (result) {
-        $scope.profileData = result.data;
-      }, function (error) {
-        alert("There was a problem getting your profile.");
-        console.log(error);
-      });
-    } else {
-        alert("Not loged in!");
-        // $scope.$emit('redirectToLoginPage', '');
-      // $rootScope.login();
-    }
-  };
 
-});
-
-// .service('LoginPage', function() {
-//   return {};
-// });
 
 
 
